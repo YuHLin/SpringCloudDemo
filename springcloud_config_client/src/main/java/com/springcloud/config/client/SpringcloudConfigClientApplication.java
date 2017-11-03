acaa4fdb61a8cd162f654c6a1f0b316aae0f2e2a
laifulin <laifulin@32gold.com>
2017-11-03 14:32:43 +0800

    Spring Cloud Bus 消息总线(结合Spring Cloud Config 是用可以动态修改配置文件)

--- springcloud_config_client/src/main/java/com/springcloud/config/client/SpringcloudConfigClientApplication.java
+++ springcloud_config_client/src/main/java/com/springcloud/config/client/SpringcloudConfigClientApplication.java
@@ -3,11 +3,20 @@ package com.springcloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
+/**
+ *
+ * @RefreshScope
+ * 标注需要自动更新配置变量的Java类
+ *
+ * 需要更新是执行  curl -X POST http://localhost:8881/bus/refresh
+ */
 public class SpringcloudConfigClientApplication {
 
 	public static void main(String[] args) {
